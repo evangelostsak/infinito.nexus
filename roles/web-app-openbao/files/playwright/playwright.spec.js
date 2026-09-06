e51e5f8c133fbe1e81d40af3270dd71a240f5d4c
evangelostsak <baggtsak55@gmail.com>
2026-09-07 00:35:24 +0200

    feat(web-app-openbao): add Playwright spec aggregator

---
+++ roles/web-app-openbao/files/playwright/playwright.spec.js
@@ -0,0 +1,8 @@
+require("./test-baseline");
+require("./test-guest-persona");
+require("./test-biber-persona");
+require("./test-administrator-persona");
+require("./test-oidc-login");
+require("./test-ldap-login");
+require("./test-rbac-denial");
+require("./test-seal-status");
